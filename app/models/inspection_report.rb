d032aed7d7e8f0acdef4db52d99a8d8976c6df60
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2023-12-21 14:11:09 -0500

    add cover image validation for inspection report

--- app/models/inspection_report.rb
+++ app/models/inspection_report.rb
@@ -3,4 +3,17 @@ class InspectionReport < ApplicationRecord
   has_one_attached :cover_image do |attachable|
     attachable.variant :thumb, resize_to_fill: [320, 240], preprocessed: true
   end
+
+  validate :acceptable_cover_image
+
+
+  private
+
+  def acceptable_cover_image
+    return unless cover_image.attached?
+    acceptable_types = ["image/jpeg", "image/png", "image/jpg"]
+    unless acceptable_types.include?(cover_image.content_type)
+      errors.add(:cover_image, "must be a JPEG or PNG")
+    end
+  end
 end
