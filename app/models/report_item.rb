323f471d68fc6ad809829932b56a1c9a4a3d42b7
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2024-01-04 18:12:07 -0500

    use local file system for dev and fix thumb url

--- app/models/report_item.rb
+++ app/models/report_item.rb
@@ -2,7 +2,7 @@ class ReportItem < ApplicationRecord
   belongs_to :inspection_report
 
   has_many_attached :item_images do |attachable|
-    attachable.variant :thumb, resize_to_fill: [320, 240], preprocessed: true
+    attachable.variant :thumb, resize_to_fill: [320, 240]
   end
 
   enum :severity, [:normal, :severe, :critical]
