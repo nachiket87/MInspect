36adf7a90131d7d420ed6dad52621501a754e889
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2024-01-18 17:53:47 -0500

    add user foreign key to inspection report

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_15_173505) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_18_224349) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_15_173505) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_inspection_reports_on_user_id"
   end
 
   create_table "report_items", force: :cascade do |t|
@@ -76,5 +78,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_15_173505) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "inspection_reports", "users"
   add_foreign_key "report_items", "inspection_reports"
 end
